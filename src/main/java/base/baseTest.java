42692d23da7583983935fcb63863f09f3a4c28c6
mohdsufiyan3755 <sufiyan.27081@gmail.com>
2025-11-28 20:49:00 +0530

    Adding Extent Report and Screenshot configuration

--- src/main/java/base/baseTest.java
+++ src/main/java/base/baseTest.java
@@ -4,36 +4,66 @@ import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.ITestResult;
 import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeSuite;
+
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.MediaEntityBuilder;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import utils.ExtentReportManager;
 import utils.log;
 
 public class baseTest {
-	
+
 	protected WebDriver driver;
-	
+	protected static ExtentReports extent;
+	protected ExtentTest test;
+
+	@BeforeSuite
+	public void setupReport() {
+		extent = ExtentReportManager.getReportInstance();
+	}
+
+	@AfterSuite
+	public void teardownReport() {
+		extent.flush();
+		// String reportPath = ExtentReportManager.reportPath;
+		// EmailUtils.sendTestReport(reportPath);
+	}
+
 	@BeforeMethod
 	public void setUp() {
-		
+
 		log.info("**********Starting webdriver**************");
 		WebDriverManager.chromedriver().setup();
 		driver = new ChromeDriver();
-		
+
 		// General setup
-        driver.manage().window().maximize();
-        //driver.manage().timeouts().implicitlyWait(3000);  // Implicit Wait
-        
-        log.info("**********Launching URL**************");
+		driver.manage().window().maximize();
+		// driver.manage().timeouts().implicitlyWait(3000); // Implicit Wait
+
+		log.info("**********Launching URL**************");
 		driver.get("https://practicetestautomation.com/practice-test-login/");
 	}
-	
+
 	@AfterMethod
-	public void tearDown() {
+	public void tearDown(ITestResult result) {
+
+		if (result.getStatus() == ITestResult.FAILURE) {
+
+			String screenshotPath = ExtentReportManager.captureScreenshot(driver, "LoginFailure");
+			test.fail("Test Failed.. Check Screenshot",
+					MediaEntityBuilder.createScreenCaptureFromPath(screenshotPath).build());
+		}
+
 		if (driver != null) {
 			log.info("**********Closing the browser**************");
-            driver.quit();
+			driver.quit();
 		}
 	}
 
--- src/test/java/tests/loginTest.java
+++ src/test/java/tests/loginTest.java
@@ -5,6 +5,7 @@ import org.testng.annotations.Test;
 
 import base.baseTest;
 import pages.loginPage;
+import utils.ExtentReportManager;
 import utils.log;
 
 public class loginTest extends baseTest {
@@ -13,15 +14,18 @@ public class loginTest extends baseTest {
 	public void verifySuccessfulLogin() {
 		// Initialize Page Object
 		log.info("**********Starting Login test**************");
+		test = ExtentReportManager.createTest("Login Test - ");
 		loginPage login = new loginPage(driver);
 		log.info("**********Entering login credentials**************");
+		test.info("Adding Credentails");
 		login.enterUsername("student");
 		login.enterPassword("Password123");
+		test.info("Clicking on Login button");
 		login.clickLoginButton();
 		log.info("**********Validating Page Title**************");
 		System.out.println("Title of the Webpage is: " + driver.getTitle());
 		Assert.assertEquals(driver.getTitle(), "Logged In Successfully | Practice Test Automation");
-		
+		test.pass("Login Successful");
 
 	}
 
